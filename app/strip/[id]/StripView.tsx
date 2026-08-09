8a4fa4c789945c33f1c8fcb751d5f8a3c0dac527
Xyrille Navora <xyrillenavora5@gmail.com>
2026-08-09 21:30:36 +0800

    Import Photostrip component from booth3d for enhanced functionality in StripView

--- app/strip/[id]/StripView.tsx
+++ app/strip/[id]/StripView.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import Button from "@/components/ui/Button";
 import OutputActions from "@/components/booth/OutputActions";
 import StickerOverlay from "@/components/booth/StickerOverlay";
+import Photostrip from "@/components/booth3d/Photostrip";
 
 export interface StripViewProps {
   stripId: string;
@@ -32,14 +33,12 @@ export default function StripView({ stripId, signedUrl }: StripViewProps) {
         </p>
       </div>
 
-      <div className="relative w-full max-w-sm animate-fade-up">
-        {/* eslint-disable-next-line @next/next/no-img-element -- signed Storage URL, not a static/optimizable asset. */}
-        <img
+      <div className="animate-fade-up">
+        <Photostrip
           src={signedUrl}
           alt="A shared photo strip"
-          className="mx-auto block w-full rounded-card-lg border border-hairline border-structural-gray bg-film-black p-2"
+          overlay={<StickerOverlay visible={stickersVisible} />}
         />
-        <StickerOverlay visible={stickersVisible} />
       </div>
 
       <Button
